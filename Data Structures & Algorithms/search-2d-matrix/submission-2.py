2dcf2faa30f6a5d0608f5c3f1f65ff2abb19c421
Ammar Salman <149531900+ammar-slmn@users.noreply.github.com>
2026-04-30 14:57:20 +0300

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.py
@@ -0,0 +1,42 @@
+"""
+Input: matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]], target = 15
+
+Output = False
+
+Input: matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]], target = 10
+
+Output: true
+
+"""
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows, cols = len(matrix), len(matrix[0])
+
+        top, bottom = 0, rows - 1
+
+        while top <= bottom: 
+            row = (top + bottom) // 2 
+            if target > matrix[row][-1]: 
+                top = row + 1 
+            elif target < matrix[row][0]: 
+                bottom = row - 1 
+            else: 
+                break 
+
+        # row is our index 
+        row = (top + bottom) // 2
+
+        l, r = 0, cols - 1
+        while l <= r: 
+            m = (l + r) // 2
+            print("this is the middle: ", m)
+            if target > matrix[row][m]: 
+                l = m + 1 
+            elif target < matrix[row][m]: 
+                r = m - 1
+            else: 
+                return True 
+        return False 
+                
+            
+        
